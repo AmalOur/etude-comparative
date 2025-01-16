e06c8814e5e23129721ff9fcdb4a90fb561333a7
Amal Our <ourajimamal@gmail.com>
2025-01-16 22:33:19 +0100

    report

--- consul/car/src/main/java/com/example/voiture/repositories/VoitureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.voiture.repositories;
-
-import com.example.voiture.entities.Voiture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoitureRepository extends JpaRepository<Voiture,Long> {
-}
